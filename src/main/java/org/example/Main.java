4f30935e618d02cecc71bfbcd655b03f880aaa45
SaveliyKorneev <savelvar@mail.ru>
2025-07-10 21:49:22 +0600

    Update Main.java
    Add sout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("привет");
         System.out.println("GIT");
+        System.out.println("Ты сможешь");
     }
 }
