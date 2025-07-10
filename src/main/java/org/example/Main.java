3416bb947d13edb499838ac1a53c74113176a530
SaveliyKorneev <savelvar@mail.ru>
2025-07-10 23:04:52 +0600

    Change sout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет");
+        System.out.println("привет мир");
         System.out.println("GIT");
         System.out.println("Ты сможешь");
         System.out.println("HAPPY");
